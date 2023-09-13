f11d9cab7675bcd6cfb7c7c9750d7fa0b305e46c
Ivan Fetko <fetkoivan2402@gmail.com>
2023-09-13 13:44:29 +0300

    Add skills component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, AboutComponent, SkillsComponent],
   imports: [BrowserModule, RouterOutlet],
   providers: [],
   bootstrap: [AppComponent],
