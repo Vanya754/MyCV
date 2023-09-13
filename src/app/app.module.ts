6b2c8a8fbf28e7e46f7eb79917c665c3476b4cc6
Ivan Fetko <fetkoivan2402@gmail.com>
2023-09-13 11:25:56 +0300

    Add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [BrowserModule, RouterOutlet],
   providers: [],
   bootstrap: [AppComponent],
